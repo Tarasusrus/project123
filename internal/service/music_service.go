3c6e1d75e9eb6bc8eed4d7c07ee8f75db4647a22
Taras Malinovskii <bar.norilsk@gmail.com>
2024-10-12 20:38:46 +0300

    Refactor MusicService and update initialization
    Reorganized MusicService by creating a new private struct and methods instead of a public struct. Added a new constructor function that properly initializes the services using a repository. This change aims to improve encapsulation and maintainability.

--- internal/service/music_service.go
+++ internal/service/music_service.go
@@ -1,8 +1,26 @@
 package service
 
-import "BaseApi/internal/service/interfaces"
+import (
+	"BaseApi/internal/service/interfaces"
+	"BaseApi/internal/service/repository"
+)
 
-type MusicService struct {
-	UserService  interfaces.UserService
-	AdminService interfaces.AdminService
+type musicService struct {
+	userService  interfaces.UserService
+	adminService interfaces.AdminService
+}
+
+func (m *musicService) UserService() interfaces.UserService {
+	return m.userService
+}
+
+func (m *musicService) AdminService() interfaces.AdminService {
+	return m.adminService
+}
+
+func NewMusicService(repository repository.Repository) interfaces.MusicService {
+	return &musicService{
+		userService:  NewUserService(repository),
+		adminService: NewAdminService(repository),
+	}
 }
