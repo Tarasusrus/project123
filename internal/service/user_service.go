ad29bdfd7314fb3d8b73015c38df3a60e9d261ea
Taras Malinovskii <bar.norilsk@gmail.com>
2024-10-12 20:39:24 +0300

    Add user service interface implementation
    Introduce `userService` struct with methods for `GetSongText` and `GetLibrary` in the user service. These methods currently contain placeholder implementations.

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"BaseApi/internal/models"
+	"BaseApi/internal/service/interfaces"
+	"BaseApi/internal/service/repository"
+	"context"
+)
+
+type userService struct {
+	Repository repository.Repository
+}
+
+func NewUserService(repository repository.Repository) interfaces.UserService {
+	return &userService{Repository: repository}
+}
+
+func (u userService) GetSongText(ctx context.Context, songID int, verse int) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userService) GetLibrary(ctx context.Context, filter models.LibraryFilter, page, pageSize int) ([]models.Song, error) {
+	//TODO implement me
+	panic("implement me")
+}
