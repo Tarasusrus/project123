55f15c1cce59a57d725b581c7290c1146d19b2c1
Taras Malinovskii <bar.norilsk@gmail.com>
2024-10-12 20:37:51 +0300

    Add admin service implementation
    Create `admin_service.go` to introduce initial admin service operations. This includes functions for deleting, updating, and adding songs, currently with placeholder implementations. This setup is a foundation for further development and integration.

--- internal/service/admin_service.go
+++ internal/service/admin_service.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"BaseApi/internal/models"
+	"BaseApi/internal/service/interfaces"
+	"BaseApi/internal/service/repository"
+	"context"
+)
+
+func NewAdminService(repository repository.Repository) interfaces.AdminService {
+	return &adminService{Repository: repository}
+}
+
+type adminService struct {
+	Repository repository.Repository
+}
+
+func (a adminService) DeleteSong(ctx context.Context, songID int) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a adminService) UpdateSong(ctx context.Context, songID int, songData models.SongUpdate) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a adminService) AddSong(ctx context.Context, songData models.NewSong) error {
+	//TODO implement me
+	panic("implement me")
+}
